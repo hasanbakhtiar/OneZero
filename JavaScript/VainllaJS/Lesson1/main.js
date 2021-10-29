44f37fae1c1c7c09024b7f0d05e7fd0243f62609
Hasan Bakhtiar <hasan.doctype@gmail.com>
2021-10-29 16:42:10 +0400

    JavaSctipt Vanilla Lesson1

---
+++ JavaScript/VainllaJS/Lesson1/main.js
@@ -0,0 +1,17 @@
+
+// document.write("Hello external js");
+
+// alert("Salam Saytımıza xoş gəldiniz...");
+// window.alert("Salam Saytımıza xoş gəldiniz...");
+
+
+
+
+// debugger;
+console.log("Hello Console1");
+console.log("Hello Console2");
+console.log("Hello Console3");
+console.log("Hello Console4");
+console.log("Hello Console5");
+console.log("Hello Console6");
+console.log("Hello Console7");
