754922b0093243d1882e8e531c9f5f5a38504738
Hasan Bakhtiar <hasan.doctype@gmail.com>
2021-11-02 17:06:40 +0400

    operatos and data type

---
+++ JavaScript/VainllaJS/Lesson2-DataTypeAndOperators/main.js
@@ -0,0 +1,56 @@
+// Prirmitive(Bəsit) Data Type (Data Tipi)
+    //string    -> mətn
+    const infoDataSt = "This type string";
+    const infoDataSt1 = "5";
+    const infoDataSt2 = "10";
+    console.log(infoDataSt1+infoDataSt2);
+
+
+    //number    -> rəqəm
+    const infoDataNb = 5;
+    const infoDataNb1 = 15;
+    const infoDataNb2 = 51;
+    console.log(infoDataNb1+infoDataNb2);
+
+
+    //boolean   -> məntiqi
+    const infoDataTr = true;
+    const infoDataFs = false;
+    console.log(infoDataTr);
+
+    if(5>10){
+            console.log("This is true ");
+    }else{
+            console.log("This is false ");
+    }
+
+
+
+// Composite(Birləşik) Data Type (Data Tipi)
+    //array     -> qutu
+    // const infoArray1 = new Array(); //method base
+    const infoArray = ["table", "pen",5,true];
+    console.log(infoArray);
+
+    //object    -> obyekt
+    // const infoObj1 = new Object(); //method base
+
+    const infoObj = {
+        name:"Rafet",
+        surname:"Mammadov"
+    };
+
+    console.log(infoObj.surname);
+
+
+// Special(Xüsusi) Data Type (Data Tipi)
+    //undefined -> tanımlanmamış
+    const infoUnf = undefined;
+    console.log(infoUnf);
+
+    
+    //null      -> boş
+    const infoNull = null;
+    console.log(infoNull);
+
+
