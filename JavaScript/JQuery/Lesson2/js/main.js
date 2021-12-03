001ee960f940365fc6b5a89cdc4d2e1f71bf1aaf
Hasan Bakhtiar <hasan.doctype@gmail.com>
2021-12-03 16:25:09 +0400

    jquery lesson2

---
+++ JavaScript/JQuery/Lesson2/js/main.js
@@ -0,0 +1,39 @@
+// $(function(){
+//     $("#flip").click(function(){
+//       $("#panel").slideToggle(1000,//()=>{
+//         //   alert("Hello")}
+//       );
+//     //   $("#panel").slideDown("slow");
+//     //   $("#panel").slideUp("slow");
+//     });
+//   });
+
+
+// $(function(){
+//     $("button").click(function(){
+//         $("div").css("color", "red").slideUp(2000).slideDown(2000);
+
+//     //   var div = $("div");  
+//     //   div.animate({bottom: '100px'}, 1000);
+//     //   div.animate({fontSize: '3em'}, 1000);
+//     //   div.animate({
+//     //     left: '250px',
+//     //     opacity: '0.5',
+//     //     height: '150px',
+//     //     width: '150px'
+//     //   }).css("background-color", "red");
+//     });
+//   });
+
+//   Stop()
+  https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_stop_slide
+
+  
+
+  var Rafet = $.noConflict();
+
+  Rafet(function(){
+    Rafet("button").click(function(){
+      alert(Rafet("#w3s").attr("href"));
+    });
+  });
