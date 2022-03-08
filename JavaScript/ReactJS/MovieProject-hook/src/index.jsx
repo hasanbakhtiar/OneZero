a77d0e1da6a01056412a9ac7baf01ec16f388ab2
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-03-08 16:47:04 +0400

    movie

---
+++ JavaScript/ReactJS/MovieProject-hook/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import './assets/sass/style.css';
+// https://v5.reactrouter.com/web/api/NavLink
+function App(){
+    return(
+        <div><AppRouter/></div>
+    )
+}
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
---
+++ JavaScript/ReactJS/MovieProject-hook/src/routers/AppRouter.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Header from '../components/Header'
+import Contact from '../components/pages/Contact'
+import Home from '../components/pages/Home'
+import MovieList from '../components/pages/MovieList'
+import notFoundPage from '../components/pages/notFoundPage'
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <div className="container">
+    <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/movies" component={MovieList}></Route>
+            <Route path="/contact" component={Contact}></Route>
+            <Route  component={notFoundPage}></Route>
+     
+    </Switch>
+    </div>
+    </BrowserRouter>
+  )
+}
+
+export default AppRouter
